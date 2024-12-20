636c327de0fbfd934ace589d0138498f53abdeec
MaliSoi <malalasoifer@gmail.com>
2024-12-20 20:20:48 -0300

    Agregar cambios

--- Trabajo2.js
+++ Trabajo2.js
@@ -49,4 +49,28 @@ mostrarTodosLosUsuarios = usuarios.map(function(usuarios){
 });
 console.log(`Asi queda la lista de usuarios:`, usuarios);
 
+//Crear una función buscarUsuario(email) que devuelva la información de un usuario dado su email.
+function  buscarUsuario(criterio, valor) {
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i][criterio] === valor) {
+            return usuarios[i]; // Devuelve el libro encontrado
+        }
+    }
+    return "No se encontraron usuarios que coincidan con esa búsqueda."; // Si no encuentra nada
+};
+
+//// Ejemplo de uso:
+console.log(`Este es el usuario encontrado según su mail:` , buscarUsuario("email", "ramonHu@gmail.com")); // Buscar por email
+
+//Implementar una función borrarUsuario(nombre, email) que elimine el usuario seleccionado.
+
+function borrarUsuario(nombre, email) {
+    // Filtra todos los usuarios cuyo nombre e email no sean igual al nombre e email pasado como parámetro.
+  const nuevaListaUsuarios = usuarios.filter(function(usuarios) {
+    return  usuarios.nombre.email !== nombre.email;
+  });
+  console.log(`El usuario con nombre: ${nombre} e email:${email} ha sido eliminado.`);
+}
+// Llamar a la función para buscar un libro por su ID
+borrarUsuario('Lara', 'LaraGala@gmail.com');
 
