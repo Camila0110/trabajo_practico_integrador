c35bffbe63e0895778025fefd174a90461dae5fd
MaliSoi <malalasoifer@gmail.com>
2024-12-20 13:24:18 -0300

    Agregar nuevas funciones al archivo Trabajo1.js

--- Trabajo1.js
+++ Trabajo1.js
@@ -13,7 +13,7 @@ const libros = [
     { id: 4, titulo: 'Cuentos sin plumas', autor: 'Woody Allen', anio: 1988, genero: ' Humor', disponible: true },
     { id: 6, titulo: 'Las palabras calladas', autor: 'Pedro Miguel Lamet Moreno', anio: 2018, genero: 'Novela Histórica ', disponible: false },
     { id: 7, titulo: 'Cien años de soledad', autor: 'Gabriel García Márquez', anio: 1967, genero: 'Realismo mágico', disponible: true },
-    { id: 8, titulo: '1984', autor: 'George Orwell', anio: 1949, genero: 'Distopía', disponible: false },
+    { id: 8, titulo: '1984', autor: 'George Orwell', anio: 1949, genero: 'Ciencia Ficción', disponible: false },
     { id: 9, titulo: 'Orgullo y prejuicio', autor: 'Jane Austen', anio: 1813, genero: 'Romance', disponible: true },
     { id: 10, titulo: 'Sapiens: De animales a dioses', autor: 'Yuval Noah Harari', anio: 2011, genero: 'No ficción', disponible: true }
 ];
@@ -52,6 +52,7 @@ let usuarios = [
 
 //2. Funciones de Gestión de Libros
 
+//Agregar Libros
 function agregarLibro(id, titulo, autor, anio, genero) {
     const nuevoLibro = { id, titulo, autor, anio, genero };
     libros.push(nuevoLibro);
@@ -61,7 +62,8 @@ function agregarLibro(id, titulo, autor, anio, genero) {
 //agregarLibro (11, "La casa de Bernarda Alba", "Federico García Lorca", 1945, "drama"); 
 //console.log(libros);
 
-function buscarLibro(criterio, valor) {
+//Buscar Libros:
+function  buscarLibro(criterio, valor) {
     for (let i = 0; i < libros.length; i++) {
         if (libros[i][criterio] === valor) {
             return libros[i]; // Devuelve el libro encontrado
@@ -73,22 +75,26 @@ function buscarLibro(criterio, valor) {
 //// Ejemplo de uso:
 //console.log(buscarLibro("titulo", "1984")); // Buscar por título
 //console.log(buscarLibro("autor", "Gabriel García Márquez")); // Buscar por autor
-//console.log(buscarLibro("genero", "Distopía")); // Buscar por género
-    
+//console.log(buscarLibro("genero", "Ciencia Ficción")); // Buscar por género
    
+//Ordenar libros según anios:
 
+function ordenarLibrosPorAnio(libros){
+    // Algoritmo de burbuja para ordenar el array 'libros' según la propiedad 'anio'
+    for (let i = 0; i < libros.length; i++) {
+        for (let j=0; j< libros.length - 1; j++){
+            // Compara los valores de la propiedad 'anio' de los objetos
+            if(libros[j].anio> libros[j+1].anio){
+            // Si el año de 'libros[j]' es mayor que el de 'libros[j + 1]', intercambiamos los objetos completos
+            let temp = libros[j];
+            libros[j]=libros[j+1];
+            libros[j+1]=temp;
+        }
+    }
+}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Llamada a la función para ordenar los libros por año
+ordenarLibrosPorAnio(libros);
+// Mostrar el resultado en consola
+console.log(libros);
